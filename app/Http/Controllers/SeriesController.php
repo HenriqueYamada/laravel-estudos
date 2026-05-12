164317e7184d51470e5fb6b5f380646e972b0ecc
Henrique Yamada <henriqueyamada01@gmail.com>
2026-05-12 16:18:07 -0300

    .

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+<<<<<<< Updated upstream
     public function index(Request $request)
     {
         $series = Serie::all();
@@ -16,6 +17,18 @@ class SeriesController extends Controller
 
         return view('series.index')->with('series', $series)
             ->with('mensagemSucesso', $mensagemSucesso);
+=======
+    public function index() {
+        //return redirect(to: 'https://google.com'); //ao colocar na url /series, a página será redirecionada para o site da google
+
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return view('series.index', compact('series'));  // função do Laravel que nos permite ir à pasta view e retornar como reposta uma string, que está no arquivo listar-series.php, tendo como segundo parâmetro 'series' que tem o valor (=>) o array $series
+    }       
+
+    public function create() {
+        return view('/series.create');
+>>>>>>> Stashed changes
     }
 
     public function create()
